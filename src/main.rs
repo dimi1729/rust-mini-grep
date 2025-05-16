51768d7a31e9bdbe1b9f6cd243813fc511f8652e
dimi1729 <dimi@tamu.edu>
2025-05-16 07:25:23 -0500

    added config type and better error handling

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,50 @@
 use std::env;
+use std::error::Error;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     println!("{:?}", args);
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = Config::new(&args);
 
-    println!("Query: {}", query);
-    println!("File path: {}", filename);
+    println!("Query: {}", config.query);
+    println!("File path: {}", config.filename);
 
-    let contents = fs::read_to_string(filename).expect("Something went wrong reading the file");
+    if let Err(e) = run(config) {
+        println!("{}", e);
+        process::exit(1);
+    }
+}
 
-    println!("File content: {}", contents)
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
+    let contents = fs::read_to_string(config.filename)?;
+
+    println!("File content: {}", contents);
+
+    Ok(())
+}
+
+struct Config {
+    query: String,
+    filename: String,
+}
+
+impl Config {
+    fn new(args: &[String]) -> Config {
+        // args[0] is the file thats actually running, 1, 2, ... are the user arguments
+        // This means we still need at least 3 args
+        assert!(
+            args.len() >= 3,
+            "Not enough arguments were provided, at least 3 are needed"
+        );
+        let query = args[1].clone();
+        let filename = args[2].clone();
+
+        return Config {
+            query: query,
+            filename: filename,
+        };
+    }
 }
